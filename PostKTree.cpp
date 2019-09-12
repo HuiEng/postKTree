ceba7813cd98d424d8625a79f1fb334d244cf4ed
HuiEng <hui_eng1995@hotmail.com>
2019-09-13 07:24:29 +1000

    Revert "kmean of ktree, distortion"
    This reverts commit 9f378a755761b975a07bbbad645e441f994c627d.

--- PostKTree.cpp
+++ PostKTree.cpp
@@ -973,6 +973,7 @@ struct KTree {
 		//vector<uint64_t> meanSigs = ktreeMeanSigs;
 		vector<size_t> clusters(inputClusters.size());
 		vector<vector<size_t>> clusterLists;
+		//int iteration = 0;
 		//while (true) {
 		int k_iteration = 20;
 		for (size_t iteration = 0; iteration < k_iteration; iteration++) {
